9fe6e40befd71716681520679b7f4b554576e448
tristaZero <panjuan@apache.org>
2020-11-19 17:15:35 +0800

    init LogicTableScan

--- src/main/java/federated/sql/schema/LogicTable.java
+++ src/main/java/federated/sql/schema/LogicTable.java
@@ -1,91 +1,29 @@
 package federated.sql.schema;
 
-import com.google.common.collect.Lists;
-import lombok.Getter;
 import org.apache.calcite.adapter.enumerable.EnumerableConvention;
 import org.apache.calcite.adapter.enumerable.EnumerableTableScan;
 import org.apache.calcite.plan.RelOptTable;
 import org.apache.calcite.rel.RelNode;
-import org.apache.calcite.rel.core.TableScan;
-import org.apache.calcite.rel.rel2sql.RelToSqlConverter;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.schema.TranslatableTable;
 import org.apache.calcite.schema.impl.AbstractTable;
-import org.apache.calcite.sql.SqlDialect;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.List;
 
 /**
  * Logic Table.
  *
  */
 public class LogicTable extends AbstractTable implements TranslatableTable {
-    
-    private LogicTableScan mysqlReader;
-    private RelToSqlConverter relToSqlConverter;
-    private String schema;
-    private String tableName;
-    private Connection connection;
 
-    @Getter
-    private RelDataType relDataType;
-    private List<RelDataType> colunmTypes;
-    private List<String> columnNames;
-
-    public LogicTable(String schema, String tableName, Connection connection) {
-        this.schema = schema;
-        this.tableName = tableName;
-        this.connection = connection;
-        relToSqlConverter = new RelToSqlConverter(SqlDialect.DatabaseProduct.MYSQL.getDialect());
-    }
-
+    @Override
     public RelNode toRel(RelOptTable.ToRelContext context, RelOptTable relOptTable) {
-        final TableScan relNode = new EnumerableTableScan(context.getCluster(),
+        return new EnumerableTableScan(context.getCluster(),
                 context.getCluster().traitSetOf(EnumerableConvention.INSTANCE),
                 relOptTable, Object[].class);
-
-        if (null == mysqlReader) {
-            final String sql = relToSqlConverter.visit(relNode).asStatement().toString();
-            mysqlReader = new LogicTableScan(sql, connection, this);
-        }
-
-        return relNode;
     }
 
+    @Override
     public RelDataType getRowType(RelDataTypeFactory typeFactory) {
-        //get meta data from mysql;
-        if (null != relDataType) {
-            return relDataType;
-        }
-
-        //try to get table meta data from db;
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement(MYSQL_COLUMN_META_SQL);
-
-            preparedStatement.setString(1, schema);
-            preparedStatement.setString(2, tableName);
-            ResultSet r = preparedStatement.executeQuery();
-
-            columnNames = Lists.newArrayList();
-            colunmTypes = Lists.newArrayList();
-            while (r.next()) {
-                //FIXME Ignore column case
-                columnNames.add(r.getString(1));
-                final String columnTypeString = r.getString(2);
-                final Class c = JavaTypeToSqlTypeConversion.getJavaTypeBySqlType(columnTypeString);
-                colunmTypes.add(typeFactory.createJavaType(c));
-            }
-
-            relDataType = typeFactory.createStructType(colunmTypes, columnNames);
-            return relDataType;
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+        return null;
     }
 }
--- src/main/java/federated/sql/schema/LogicTableScan.java
+++ src/main/java/federated/sql/schema/LogicTableScan.java
@@ -1,83 +1,8 @@
 package federated.sql.schema;
 
-import com.google.common.collect.Lists;
-import com.yuqi.schema.common.util.ResultSetUtils;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.apache.calcite.rel.type.RelDataType;
-import org.apache.calcite.rel.type.RelDataTypeFactoryImpl;
-import org.apache.calcite.rel.type.RelDataTypeField;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 /**
  * Logic rows reader
  *
  */
-@Getter
-@Builder
-@AllArgsConstructor
-@RequiredArgsConstructor
-public class LogicTableScan {
-
-    @Setter
-    @Getter
-    private String sql;
-    private final Connection connection;
-    private final LogicTable logicTable;
-
-    public Iterator<Object[]> readData() {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement(sql);
-            ResultSet r = preparedStatement.executeQuery();
-            RelDataType relDataType = logicTable.getRelDataType();
-
-            return getDataFromResult(r, relDataType);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    private Iterator<Object[]> getDataFromResult(ResultSet r, RelDataType colunmType) {
-        final List<RelDataTypeField> f = colunmType.getFieldList();
-        //RelDataTypeFactoryImpl.JavaType
-
-        List<Class> typeClass = f.stream()
-                .map(RelDataTypeField::getType)
-                .map(a -> (RelDataTypeFactoryImpl.JavaType) a)
-                .map(RelDataTypeFactoryImpl.JavaType::getJavaClass)
-                .collect(Collectors.toList());
-
-
-        final List<Object[]> result = Lists.newArrayList();
-        final int columnSize = typeClass.size();
-
-        try {
-            final List<Class> columnTypes = ResultSetUtils.getColumnTypeFromResultSet(r);
-
-            while (r.next()) {
-                final Object[] objects = new Object[columnSize];
-                for (int i = 1; i <= columnSize; i++) {
-                    objects[i - 1] = ResultSetUtils.javaTypeToObject(r, i, columnTypes.get(i - 1));
-                }
-
-                result.add(objects);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return result.iterator();
-    }
-
+public final class LogicTableScan {
 }
